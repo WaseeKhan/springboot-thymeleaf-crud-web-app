026ba570f79d2465c22af0cf144191ce4bf58f52
Waseem <mdvaseem014@gmail.com>
2023-09-06 22:01:53 +0530

    SpringBootServletInitializer added in Main class

--- src/main/java/com/lucifer/lab/ThymeleafDemoApplication.java
+++ src/main/java/com/lucifer/lab/ThymeleafDemoApplication.java
@@ -2,9 +2,10 @@ package com.lucifer.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThymeleafDemoApplication {
+public class ThymeleafDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafDemoApplication.class, args);
